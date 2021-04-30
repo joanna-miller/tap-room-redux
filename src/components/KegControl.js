434c83212251cdd0b0dfeb9a33a48bed6ae69a74
Jo <joannadawnmiller@gmail.com>
2021-04-30 13:06:28 -0400

    incorporate TOGGLE_FORM reducer

--- src/components/KegControl.js
+++ src/components/KegControl.js
@@ -11,7 +11,6 @@ class KegControl extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      formVisibleOnPage: false,
       selectedKeg: null
     };
   }
@@ -19,12 +18,14 @@ class KegControl extends React.Component {
   handleClick = () => {
     if (this.state.selectedKeg != null) {
       this.setState({
-        formVisibleOnPage: false,
         selectedKeg: null
       });
     } else { 
-      this.setState(prevState => ({
-      formVisibleOnPage: !prevState.formVisibleOnPage }));
+      const { dispatch } = this.props;
+      const action = {
+        type: 'TOGGLE_FORM'
+      }
+      dispatch(action);
     }
   }
 
@@ -41,7 +42,10 @@ class KegControl extends React.Component {
       remainingStock: remainingStock
     }
     dispatch(action);
-    this.setState({formVisibleOnPage: false});
+    const action2 = {
+      type: 'TOGGLE_FORM'
+    }
+    dispatch(action2);
   }
 
   handleChangingSelectedKeg = (id) => {
@@ -82,7 +86,7 @@ class KegControl extends React.Component {
     if (this.state.selectedKeg != null) {
       currentlyVisibleState = <KegDetail keg={this.state.selectedKeg} onClickingDelete={this.handleDeletingTicket} />
       buttonText = "Return to Keg List"
-    } else if (this.state.formVisibleOnPage){
+    } else if (this.props.formVisibleOnPage){
       currentlyVisibleState = <NewKegForm onNewKegCreation={this.handleAddingNewKegToList} />
       buttonText = "Return to Keg List"
     } else {
@@ -101,12 +105,14 @@ class KegControl extends React.Component {
 }
 
 KegControl.propTypes = {
-  masterKegList: PropTypes.object
+  masterKegList: PropTypes.object,
+  formVisibleOnPage: PropTypes.bool
 };
 
 const mapStateToProps = state => {
   return {
-    masterKegList: state
+    masterKegList: state.masterKegList,
+    formVisibleOnPage: state.formVisibleOnPage
   }
 }
 
