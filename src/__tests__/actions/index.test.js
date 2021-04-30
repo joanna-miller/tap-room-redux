96860e620920d3d71a1c834652ee2f4d301ffb5c
Jo <joannadawnmiller@gmail.com>
2021-04-30 16:20:16 -0400

    pass tests for action creators

---
+++ src/__tests__/actions/index.test.js
@@ -0,0 +1,33 @@
+import * as actions from './../../actions';
+
+describe('help queue actions', () => {
+  it('deleteKeg should create DELETE_KEG action', () => {
+    expect(actions.deleteKeg(1)).toEqual({
+      type: 'DELETE_KEG',
+      id: 1
+    });
+  });
+  it('toggleForm should create TOGGLE_FORM action', () => {
+    expect(actions.toggleForm()).toEqual({
+      type: 'TOGGLE_FORM'
+    });
+  });
+  it('addKeg should create ADD_KEG action', () => {
+    expect(actions.addKeg({
+      name: 'a beer',
+      brand: 'beer brand',
+      price: '$6',
+      alcoholContent: '5%',
+      remainingStock: 124,
+      id: 1
+    })).toEqual({
+      type: 'ADD_KEG',
+      name: 'a beer',
+      brand: 'beer brand',
+      price: '$6',
+      alcoholContent: '5%',
+      remainingStock: 124,
+      id: 1
+    });
+  });
+});
---
+++ src/actions/index.js
@@ -0,0 +1,14 @@
+export const deleteKeg = id => ({ type: 'DELETE_KEG', id });
+export const toggleForm = () => ({ type: 'TOGGLE_FORM'});
+export const addKeg = (keg) => {
+  const { name, brand, price, alcoholContent, remainingStock, id } = keg;
+  return {
+    type: "ADD_KEG",
+    name: name,
+    brand: brand,
+    price: price,
+    alcoholContent: alcoholContent,
+    remainingStock: remainingStock,
+    id: id
+  }
+}
